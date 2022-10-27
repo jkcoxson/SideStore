0b0c6543f26e664cb001af299d7b304af8ec06e7
Jackson Coxson <jkcoxson@gmail.com>
2022-10-27 00:45:13 -0600

    Add minimuxer library to Xcode

---
+++ minimuxer/minimuxer.swift
@@ -0,0 +1,14 @@
+//
+//  minimuxer.swift
+//  minimuxer
+//
+//  Created by Jackson Coxson on 10/27/22.
+//
+
+import Foundation
+
+public func start_minimuxer(pairing_file: String) {
+    let pf = NSString(string: pairing_file)
+    let pf_pointer = UnsafeMutablePointer<CChar>(mutating: pf.utf8String)
+    minimuxer_c_start(pf_pointer)
+}
