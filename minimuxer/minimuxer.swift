bc38066c8878cd80930a589d9821a8d08cef4c8b
Jackson Coxson <jkcoxson@gmail.com>
2022-10-27 02:14:21 -0600

    Enable JIT on-device

--- minimuxer/minimuxer.swift
+++ minimuxer/minimuxer.swift
@@ -7,6 +7,11 @@
 
 import Foundation
 
+public enum Uhoh: Error {
+    case Good
+    case Bad
+}
+
 public func start_minimuxer(pairing_file: String) {
     let pf = NSString(string: pairing_file)
     let pf_pointer = UnsafeMutablePointer<CChar>(mutating: pf.utf8String)
@@ -15,4 +20,13 @@ public func start_minimuxer(pairing_file: String) {
 
 public func set_usbmuxd_socket() {
     target_minimuxer_address()
+}
+
+public func debug_app(app_id: String) throws -> Uhoh {
+    let ai = NSString(string: app_id)
+    let ai_pointer = UnsafeMutablePointer<CChar>(mutating: ai.utf8String)
+    if minimuxer_debug_app(ai_pointer) == -1 {
+        throw Uhoh.Bad
+    }
+    return Uhoh.Good
 }
